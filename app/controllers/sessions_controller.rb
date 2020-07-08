42c1d2e5e58523f39d8667985fc06ebd7da09a98
suika <63850313+AS-dfg@users.noreply.github.com>
2020-07-08 09:33:55 +0000

    アカウントのアクティベーションを追加する

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,18 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message  = "アカウントが有効化されてません "
+        message += "メールで有効化を確認してください."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = "メール／パスワードが有効ではありません"
       render 'new'
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "メールを確認してアカウントを有効にしてください"
+      redirect_to root_url
     else
       render 'new'
     end
